05768582f9231ecdf5290d6cbcd3df7c084a7208
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-11-10 17:25:33 +0100

    post

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: { config: './tailwindcss-config.js' },
+    tailwindcss: {},
+    autoprefixer: {} // Add this line for future CSS features
   },
-}
+};
