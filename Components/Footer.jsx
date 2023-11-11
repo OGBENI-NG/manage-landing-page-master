1c3debde3bb545522865baad81906b464b54c171
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-11-11 01:29:12 +0100

    ww

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -3,7 +3,7 @@ import CustomButton from './CustomButton'
 
 export default function Footer({email, handleChange, handleSubmit, error}) {
   return (
-    <footer  className='bg-veryDarkBlue py-10'>
+    <footer  className='bg-veryDarkBlue'>
       <form onSubmit={handleSubmit} className='px-5'>
         <div className='flex items-center gap-2 h-auto'>
           <input placeholder='Updates in your inbox…' className={`w-full h-full outline-none py-4 rounded-full px-4 text-lg ${error && "text-brightRed border-2 border-brightRed"}`} type="text" value={email} onChange={handleChange}/>
