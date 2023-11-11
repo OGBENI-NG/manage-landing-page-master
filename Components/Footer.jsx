2ac21910ac2545ee5090b49ffb8ed1b6cd6c39ca
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-11-11 16:15:34 +0100

    fixed

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -7,13 +7,13 @@ export default function Footer({email, logoFooter, handleChange, handleSubmit, e
       <form onSubmit={handleSubmit} className='px-5'>
         <div className='flex items-center gap-2 pt-8'>
           <input placeholder='Updates in your inbox…' className={`w-full outline-none py-2 rounded-full px-4 text-lg ${error && "text-brightRed border-2 border-brightRed"}`} type="text" value={email} onChange={handleChange}/>
-          <button type='submit' className="rounded-full bg-brightRed my-0 uppercase px-7 py-2 font-medium text-veryLightGray text-lg" >go</button>
+          <button type='submit' className="rounded-full bg-brightRed my-0 uppercase px-6 py-2 font-medium text-veryLightGray text-lg" >go</button>
         </div>
         <div className='pl-5 pt-1 block'>
           {error && <span className='text-brightRed italic '>{error}</span>}
         </div>
       </form>
-      <nav className='flex justify-around py-10 items-top '>
+      <nav className='flex justify-center py-10 items-top gap-20'>
         <div className='text-base leading-9 text-veryLightGray'>
           <ul><li><a href="#">Home</a></li></ul>
           <ul><li><a href="#">Pricing</a></li></ul>
@@ -26,7 +26,7 @@ export default function Footer({email, logoFooter, handleChange, handleSubmit, e
           <ul><li><a href="#">PrivacyPolicy</a></li></ul>
         </div>
       </nav>
-      <div className='text-veryLightGray flex gap-6 justify-center text-4xl'>
+      <div className='text-veryLightGray flex gap-8 justify-center text-4xl'>
           <FaSquareFacebook/>
           <FaSquareYoutube/>
           <FaSquareXTwitter/>
